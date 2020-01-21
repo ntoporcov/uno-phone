e04701ac59fee75395ba39e3321b4ccbdf4008c5
Nicolas Toporcov <ntoporcov@me.com>
2020-01-21 12:16:38 -0500

    Updates

---
+++ precache-manifest.ebd9bb6c430fea90d015ad38257e82a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c772c21853df05f02b5db851a4dd8f",
+    "url": "/uno-phone/index.html"
+  },
+  {
+    "revision": "d28061a59d65ea461f95",
+    "url": "/uno-phone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a47085c141b4c460060",
+    "url": "/uno-phone/static/js/2.d5cbbc32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/uno-phone/static/js/2.d5cbbc32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d28061a59d65ea461f95",
+    "url": "/uno-phone/static/js/main.05eda43e.chunk.js"
+  },
+  {
+    "revision": "091d026a2d31c986d4d2",
+    "url": "/uno-phone/static/js/runtime-main.35dec3bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uno-phone/static/media/logo.5d5d9eef.svg"
+  }
+]);
