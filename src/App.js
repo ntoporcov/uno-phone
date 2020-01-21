38e4c537adc7da2bfcb8fa44ab87a881e06978bc
Nicolas Toporcov <ntoporcov@me.com>
2020-01-21 16:05:52 -0500

    first couple things are done on the controller part, still need to integrate uno engine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class NumberInput extends React.Component{
+  render() {
+    return(
+        <div className={"oneNumberInput"+(this.props.focus?" focus":"")}>{this.props.number}</div>
+    )
+  }
+}
+
+class UnoInput extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      numbers:[null,null,null,null,null,null,null,null,null,null,],
+      currentIndex:0,
+    }
+  }
+
+  render() {
+    const numbersArray = this.state.numbers;
+
+    return (
+        <div>
+          <h1>Please Enter Your Phone Number</h1>
+          <p style={{fontSize:12,lineHeight:0}}>By Playing UNO</p>
+          <div className={"inputRow"}>
+            {
+              numbersArray.map((number,key) => <NumberInput key={key} number={number} focus={ key===this.state.currentIndex}/>)
+            }
+          </div>
+        </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This will be the uno phone.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UnoInput/>
     </div>
   );
 }
