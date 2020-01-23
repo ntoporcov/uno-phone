d5c266a1a0fdc7710c2ac225da763051291521be
Nicolas Toporcov <ntoporcov@me.com>
2020-01-23 08:17:43 -0500

    adding meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import UnoInput from "./UnoInput";
+import MetaTags from 'react-meta-tags';
+
 
 function App() {
   return (
     <div className="App">
+        <MetaTags>
+            <title>Phone Entry By Uno</title>
+            <meta name="description" content="Enter your phone number by playing Uno" />
+            <meta property="og:title" content="Phone Entry By Uno" />
+        </MetaTags>
       <UnoInput/>
     </div>
   );
